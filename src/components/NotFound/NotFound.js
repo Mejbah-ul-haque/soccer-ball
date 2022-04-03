99a128a73bc010c43157feb9de0ffe92efdd1d15
Mejbah <mejba.s@gmail.com>
2022-04-04 00:33:09 +0600

    connect with app.js and others pages

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Sorry!!!</h2>
+            <h3>This page is not found!!!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
