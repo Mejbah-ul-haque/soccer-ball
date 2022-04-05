02b7ac0ca161e0d54a0617012a38adf87920b80d
Mejbah <mejba.s@gmail.com>
2022-04-05 09:50:48 +0600

    Responsive navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div>
             <h2 className='text-4xl bg-green-500 p-4 text-white font-bold '>SOCCER BALL</h2>
             <nav className='border-b-2 border-green-500'>
-                <ul>
-                    <li className='flex justify-center my-5'>
-                        <CustomLink className='mr-8' to='/'>HOME</CustomLink>
-                        <CustomLink className='mr-8' to='/reviews'>REVIEWS</CustomLink>
-                        <CustomLink className='mr-8' to='/dashboard'>DASHBOARD</CustomLink>
-                        <CustomLink className='mr-8' to='/blogs'>BLOGS</CustomLink>
-                        <CustomLink className='mr-8' to='/about'>ABOUT</CustomLink>
-                    </li>
+                <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                    {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+                </div>
+                <ul className={`md:flex justify-center bg-white my-4 w-full md:static absolute duration-500 ease-in ${open ? 'top-20' : 'top-[-140px]'}`}>
+
+                    <CustomLink className='mr-8' to='/'>HOME</CustomLink>
+                    <CustomLink className='mr-8' to='/reviews'>REVIEWS</CustomLink>
+                    <CustomLink className='mr-8' to='/dashboard'>DASHBOARD</CustomLink>
+                    <CustomLink className='mr-8' to='/blogs'>BLOGS</CustomLink>
+                    <CustomLink className='mr-8' to='/about'>ABOUT</CustomLink>
+
                 </ul>
             </nav>
         </div>
