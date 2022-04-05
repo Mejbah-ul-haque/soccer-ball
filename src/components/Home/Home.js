7b92f94a91b5e900cfadf28169d6de2d99b1e73a
Mejbah <mejba.s@gmail.com>
2022-04-05 10:04:56 +0600

    Home page margin changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
     return (
         <div>
-            <div className='container mx-auto mt-8 grid md:grid-cols-2 grid-cols-1 gap-4 mx-4'>
+            <div className='container mx-auto mt-8 grid md:grid-cols-2 grid-cols-1 gap-4 my-4'>
                 <div className='mt-8 mx-8 mb-8 md:text-left'>
                     <h2 className='text-5xl font-bold text-gray-500 md:mt-8 lg:text-6xl '>A High Quality <br /> <span className='text-green-600'>Soccer Ball</span>
                     </h2>
